cb36804613f1c6d3f0ab15bbff6bfcd6b2779800
KexiMleko <ocokolja.aleksa@gmail.com>
2026-01-03 00:56:21 +0100

    Refactor: remove instr_parser.c, clean compiler command string
    Deleted instr_parser.c due to design changes. Instead of being
    output-driven, the simulator will now follow the RISC-V datapath
    and control path. Instructions will not be parsed directly; they
    will propagate through the control path.
    
    instr_fields.h will also be updated in subsequent commits.
    
    Use stack allocation for compiler command string because it will
    continue to have static length for now.

--- src/instr_fields.h
+++ src/instr_fields.h
@@ -1,19 +1,20 @@
+#pragma once
 #include <stdint.h>
-static inline uint32_t opcode(uint32_t instr) { return instr & 0x7F; }
-static inline uint32_t rd(uint32_t instr) { return (instr >> 7) & 0x1F; }
-static inline uint32_t funct3(uint32_t instr) { return (instr >> 12) & 0x07; }
-static inline uint32_t rs1(uint32_t instr) { return (instr >> 15) & 0x1F; }
-static inline uint32_t rs2(uint32_t instr) { return (instr >> 20) & 0x1F; }
-static inline uint32_t funct7(uint32_t instr) { return instr >> 25; }
-static inline int32_t i_imm(uint32_t instr) { return (int32_t)(instr >> 20); }
-static inline int32_t j_imm(uint32_t instr) {
+static inline uint32_t get_opcode(uint32_t instr) { return instr & 0x7F; }
+static inline uint32_t get_rd(uint32_t instr) { return (instr >> 7) & 0x1F; }
+static inline uint32_t get_funct3(uint32_t instr) { return (instr >> 12) & 0x07; }
+static inline uint32_t get_rs1(uint32_t instr) { return (instr >> 15) & 0x1F; }
+static inline uint32_t get_rs2(uint32_t instr) { return (instr >> 20) & 0x1F; }
+static inline uint32_t get_funct7(uint32_t instr) { return instr >> 25; }
+static inline int32_t get_i_imm(uint32_t instr) { return (int32_t)(instr >> 20); }
+static inline int32_t get_j_imm(uint32_t instr) {
   int32_t imm = 0;
   imm |= ((instr >> 31) & 0x1) << 20 | ((instr >> 12) & 0xFF) << 12 |
          ((instr >> 20) & 0x1) << 11 | ((instr >> 21) & 0x3FF) << 1;
   return (int32_t)(imm << 11) >> 11;
 }
-static inline int32_t u_imm(uint32_t instr) { return (int32_t)instr >> 12; }
-static inline int32_t b_imm(uint32_t instr) {
+static inline int32_t get_u_imm(uint32_t instr) { return (int32_t)instr >> 12; }
+static inline int32_t get_b_imm(uint32_t instr) {
   uint32_t lower_five = (instr >> 7) & 0x1F;
   uint32_t upper_seven = (instr >> 25) << 7;
   uint32_t msb = upper_seven >> 6;
@@ -23,7 +24,7 @@ static inline int32_t b_imm(uint32_t instr) {
   msb = ((msb << 1) | lsb) << 12;
   return (uint32_t)(msb | upper_seven | lower_five);
 }
-static inline int32_t s_imm(uint32_t instr) {
+static inline int32_t get_s_imm(uint32_t instr) {
   uint32_t lower_five = (instr >> 7) & 0x1F;
   uint32_t upper_seven = (instr >> 25) << 5;
   return (int32_t)(upper_seven | lower_five);
--- src/main.c
+++ src/main.c
@@ -1,28 +1,29 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #ifdef _WIN32
-   #define SH_SEP " && "
-   #define GCC "riscv-none-elf-gcc"
-   #define OBJCOPY "riscv-none-elf-objcopy"
-#else 
-   #define SH_SEP " ; "
-   #define GCC "riscv64-elf-gcc"
-   #define OBJCOPY "riscv-elf-objcopy"
+#define SH_SEP " && "
+#define GCC "riscv-none-elf-gcc"
+#define OBJCOPY "riscv-none-elf-objcopy"
+#else
+#define SH_SEP " ; "
+#define GCC "riscv64-elf-gcc"
+#define OBJCOPY "riscv-elf-objcopy"
 #endif
 
 int load_instructions(char *file_path, uint32_t *instr_mem) {
   if (file_path[strlen(file_path) - 1] == 's') {
-
-    char *cmd_buffer = malloc(512);
-    snprintf(cmd_buffer, 512,
+    size_t cmd_len=512;
+    char cmd_buffer[cmd_len] = {};
+    snprintf(cmd_buffer, cmd_len,
              "%s -march=rv32i -mabi=ilp32 -nostdlib -Ttext 0x0 %s "
              "-o output.elf%s"
              "%s -O binary -j .text output.elf output.bin",
-             GCC,file_path,SH_SEP,OBJCOPY);
-   printf("\nCompile and disassemble invoked via:\n      %s",cmd_buffer);
+             GCC, file_path, SH_SEP, OBJCOPY);
+    printf("\nCompile and disassemble invoked via:\n      %s", cmd_buffer);
     int res = system(cmd_buffer);
     if (res != 0) {
       perror("Error compiling file");
@@ -36,14 +37,13 @@ int load_instructions(char *file_path, uint32_t *instr_mem) {
     return 1;
   }
   int instr_count = fread(instr_mem, sizeof(uint32_t), 1024, file);
-   for(int i=0;i<instr_count;i++){
-      printf("\n%x",instr_mem[i]);
-   }
+  printf("\nLoaded %d instructions into instruction memory", instr_count);
   fclose(file);
   return 0;
 }
 int main(int argc, char *args[]) {
-  uint32_t regs[32] = {0};
+  uint32_t gp_regs[32] = {0};
+  uint32_t pc = 0;
   uint32_t instr_mem[1024] = {0};
   uint32_t data_mem[1024] = {0};
   load_instructions(args[1], instr_mem);
