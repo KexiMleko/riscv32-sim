a5de612669ef216a8f6a92bd3e3e6b269605e7e2
KexiMleko <ocokolja.aleksa@gmail.com>
2026-01-05 12:51:10 +0100

    refactor: use unsigned types for register bank write value and fields

--- src/register_bank.c
+++ src/register_bank.c
@@ -1,4 +1,5 @@
 #include "./register_bank.h"
+#include <stdio.h>
 
 static uint32_t gp_regs[32] = {0};
 static register_bank r_bank = {0};
@@ -9,7 +10,7 @@ void send_register_bank_input(uint32_t rs1_addr, uint32_t rs2_addr,
   r_bank.rs2_addr = rs2_addr;
   r_bank.rs1_data = gp_regs[rs1_addr];
   r_bank.rs2_data = gp_regs[rs2_addr];
-  r_bank.rd_addr=rd_addr;
+  r_bank.rd_addr = rd_addr;
   r_bank.rd_we = rd_we;
 }
 
@@ -19,10 +20,11 @@ register_bank_output get_register_bank_output() {
   rb_out.rs2_data = r_bank.rs2_data;
   return rb_out;
 }
-void rd_write(int32_t value) {
+void rd_write(uint32_t value) {
   if (r_bank.rd_we) {
     if (r_bank.rd_addr == 0)
       return;
     gp_regs[r_bank.rd_addr] = value;
+    printf("\nWrite on x%d value: %d\n", r_bank.rd_addr, gp_regs[r_bank.rd_addr]);
   }
 }
