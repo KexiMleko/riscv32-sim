360e25227b4bef35b94af8d1949b5cced5771f45
KexiMleko <ocokolja.aleksa@gmail.com>
2026-03-07 21:30:04 +0100

    refactor: remove reverse-order execution of the pipeline stages
    Separate sequential from combinational logic by removing reverse order
    execution of the pipeline stages and perform "register" writes after all
    stages have finished with execution at the end of each loop itteration.

--- src/in_order/in_order_pipeline.c
+++ src/in_order/in_order_pipeline.c
@@ -10,7 +10,7 @@
 bool run_inorder_pipeline(int32_t PC, instr_memory *instr_mem,
                           data_memory *data_mem) {
 
-  uint64_t clk_cycle = 0;
+  uint64_t clk_cnt = 0;
 
   IF_ID if_id_reg = {0};
   ID_EX id_ex_reg = {0};
@@ -21,28 +21,33 @@ bool run_inorder_pipeline(int32_t PC, instr_memory *instr_mem,
 
   printf("\nPress Enter to cycle...\n");
   while (!halt) {
-    clk_cycle++;
+    clk_cnt++;
     if (!mem_wb_reg.halt_signal) {
       fflush(stdout);
       (void)getchar();
     }
 
-    if (clk_cycle >= MAX_CLK_COUNT) {
+    if (clk_cnt >= MAX_CLK_COUNT) {
       printf("\nMax clock cycle count reached: Stopping simulation\n");
       return EXIT_FAILURE;
     }
+    IF_ID if_id_next = instr_fetch(if_id_reg, instr_mem, PC);
+    ID_EX id_ex_next = instr_decode(if_id_reg, &b_ctrl);
+    EX_MEM ex_mem_next = execute(id_ex_reg);
+    MEM_WB mem_wb_next = memory_access(ex_mem_reg, data_mem);
     halt = write_back(mem_wb_reg);
-    mem_wb_reg = memory_access(ex_mem_reg, data_mem);
-    ex_mem_reg = execute(id_ex_reg);
 
-    id_ex_reg = instr_decode(if_id_reg, &b_ctrl);
-    if_id_reg = instr_fetch(if_id_reg, instr_mem, PC);
     /*
      * if_id_reg.b_ctrl = b_ctrl is here to provide the illusion of instr_decode
      * branch evaluation in cycle t and instr_fetch seeing that change through a
      * register in cycle t+1.
      */
+    if_id_reg = if_id_next;
     if_id_reg.b_ctrl = b_ctrl;
+
+    id_ex_reg = id_ex_next;
+    ex_mem_reg = ex_mem_next;
+    mem_wb_reg = mem_wb_next;
 
     PC = if_id_reg.pc;
   }
